ab82617b316a48bf4c539a426c9a9dc4e6896948
Ziyi Chen <zc2913@nyu.edu>
2023-09-27 15:45:49 -0400

    mouse click, mouse move, key press

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,29 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("gold");
+  fill(0);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+
+}
+
+function mouseClicked() {
+  fill(random(100,200),0,0);
+  rect(mouseX, mouseY, 80, 80);
+
+}
+function mouseMoved(){
+  stroke(0);
+  line (mouseX, mouseY, 0,0);
+
+}
+
+function keyPressed(){
+  if(key == "s"){
+    saveCanvas("my canvas","jpg");
+  } else if(key == "r"){
+    background("gold");
+  }
+
 }
